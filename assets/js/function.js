5965d8cfa2f031c35a866d1b2f8201153ef858e6
vyacheslav sykhanov <nogamegolife@gmail.com>
2016-06-02 07:57:25 +0400

    1.3.1 - vanilla js

--- assets/js/function.js
+++ assets/js/function.js
@@ -5,16 +5,6 @@
   navigationNodes();
 
 
-  // HIDE SCROLL
-  //-----------------------------------------
-  function hideScroll() {
-    var parent = $('.page-wrap'),
-        child = $('.sections');
-
-    child.style.paddingRight = child.offsetWidth - child.clientWidth + 'px';
-  }
-
-
   // PARALLAX.JS
   //-----------------------------------------
   function parallaxJS() {
@@ -32,15 +22,25 @@
   // PARALLAX ON SCROLL
   //----------------------------------
   function parallaxElement() {
-    $(window).scroll(function () {
 
-      var wScroll = $(document).scrollTop();
+    window.addEventListener('scroll', function () {
+      var wScroll = window.pageYOffset;
 
-      $('.header-title').css({
-        transform: 'translateY(' + wScroll / 6 + '%)'
-      });
-
+      var headerTitle = document.querySelector('.header-title');
+      headerTitle.style.transform = 'translateY(' + wScroll / 6 + '%)';
     });
+
+    // with jQuery
+    //-------------------------------------------
+    // $(window).scroll(function () {
+    //
+    //   var wScroll = $(document).scrollTop();
+    //
+    //   $('.header-title').css({
+    //     transform: 'translateY(' + wScroll / 6 + '%)'
+    //   });
+    //
+    // });
   }
 
 
@@ -48,99 +48,119 @@
   //------------------------------------
   function navigationNodes() {
 
-    var $root = $('html, body');
+    // var root = document.querySelector('body');
+    var forEach = Array.prototype.forEach; // link on forEach function
+    var nodes = document.querySelectorAll('#nav .node');
     var positions = getElementsHeight();
 
-    $('#nav .node').click(function(e) {
-      var $this = $(this),
-          siblings = $this.parent().children(),
-          elPosition = siblings.index($this);
+    // remove class
+    function removeClassFromNodes() {
+      forEach.call(nodes, function (elem) {
+        elem.classList.remove('active');
+      });
+    }
 
-      siblings.removeClass('active')
-              .eq(elPosition)
-              .addClass('active');
+    // change class 'active' on click
+    forEach.call(nodes, function (elem, i) {
+      elem.addEventListener('click', function() {
 
+        removeClassFromNodes();
 
-      $root.animate({
-        scrollTop: positions[elPosition]
-      }, 600);
+        this.classList.add('active');
+
+        // How write this in Vanilla JS???
+        //------------------------------------
+        $('html, body').animate({
+          scrollTop: positions[i]
+        }, 600);
+
+      });
+
     });
 
+    // change class 'active' on scroll
+    window.addEventListener('scroll', function() {
+      var wScroll = window.pageYOffset;
 
-    $(window).scroll(function () {
-      var wScroll = $(document).scrollTop();
-      var nodes = $('#nav').children();
-
-      // I can't optimize this code...
-      if(wScroll >= positions[0]) {
-        nodes.removeClass('active')
-             .eq(0)
-             .addClass('active');
-      }
-      if(wScroll >= positions[1]) {
-        nodes.removeClass('active')
-             .eq(1)
-             .addClass('active');
-      }
-      if(wScroll >= positions[2]) {
-        nodes.removeClass('active')
-             .eq(2)
-             .addClass('active');
-      }
       if(wScroll >= positions[3]) {
-        nodes.removeClass('active')
-             .eq(3)
-             .addClass('active');
+        removeClassFromNodes();
+        nodes[3].classList.add('active');
       }
+      else if(wScroll >= positions[2]) {
+        removeClassFromNodes();
+        nodes[2].classList.add('active');
+      }
+      else if(wScroll >= positions[1]) {
+        removeClassFromNodes();
+        nodes[1].classList.add('active');
+      } else {
+        removeClassFromNodes();
+        nodes[0].classList.add('active');
+      }
+
     });
+
 
   }
 
 
-  // NAVIGATION old
-  //----------------------------------
-  function navigationPage() {
-    var $root = $('html, body');
-    var positions = getElementsHeight();
-    // var positions = [0, 955, 1910, 2865];
-    var currentPosition = getCurrentPosition();
-
-    $(window).scroll(function () {
-      currentPosition = getCurrentPosition();
-    });
-
-    $('.button-down').click(function () {
-
-      if(currentPosition < positions.length - 1) {
-        currentPosition++;
-
-        $root.animate({
-          scrollTop: positions[currentPosition]
-        });
-      }
-
-    });
-
-    $('.button-up').click(function () {
-
-      if(currentPosition > 0) {
-        currentPosition--;
-
-        $root.animate({
-          scrollTop: positions[currentPosition]
-        });
-      }
-
-    });
-
-  }
+  // function navigationNodes() {
+  //
+  //   var $root = $('html, body');
+  //   var positions = getElementsHeight();
+  //
+  //   $('#nav .node').click(function(e) {
+  //     var $this = $(this),
+  //         siblings = $this.parent().children(),
+  //         elPosition = siblings.index($this);
+  //
+  //     siblings.removeClass('active')
+  //             .eq(elPosition)
+  //             .addClass('active');
+  //
+  //
+  //     $root.animate({
+  //       scrollTop: positions[elPosition]
+  //     }, 600);
+  //   });
+  //
+  //
+  //   $(window).scroll(function () {
+  //     var wScroll = $(document).scrollTop();
+  //     var nodes = $('#nav').children();
+  //
+  //     // I can't optimize this code...
+  //     if(wScroll >= positions[0]) {
+  //       nodes.removeClass('active')
+  //            .eq(0)
+  //            .addClass('active');
+  //     }
+  //     if(wScroll >= positions[1]) {
+  //       nodes.removeClass('active')
+  //            .eq(1)
+  //            .addClass('active');
+  //     }
+  //     if(wScroll >= positions[2]) {
+  //       nodes.removeClass('active')
+  //            .eq(2)
+  //            .addClass('active');
+  //     }
+  //     if(wScroll >= positions[3]) {
+  //       nodes.removeClass('active')
+  //            .eq(3)
+  //            .addClass('active');
+  //     }
+  //   });
+  //
+  // }
 
 
   // THE HEIGHT OF OUR ELEMENTS(#header, #create, #paint, #enjoy)
   //---------------------------------
   function getElementsHeight() {
     var arr = [];
-    var height = $('section').height();
+    // var height = $('section').height();
+    var height = document.querySelector('section').clientHeight;
 
     for(var i = 0; i < 4; i++) {
       arr.push(height * i);
@@ -154,7 +174,8 @@
   //---------------------------------
   function getCurrentPosition() {
     var position = getElementsHeight();
-    var scrollPosition = $(document).scrollTop();
+    // var scrollPosition = $(document).scrollTop();
+    var scrollPosition = window.pageYOffset;
 
     if(scrollPosition >= position[3]) {
       position = 3;
@@ -168,7 +189,5 @@
 
     return position;
   }
-
-
 
 })();
